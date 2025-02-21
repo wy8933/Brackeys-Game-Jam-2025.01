74da52ad92ec05acfc9781dbd5671f0e1b131f64
pnaser74 <p.naser74@gmail.com>
2025-02-21 00:04:05 -0800

    add clock and fireplace sounds

--- Brackeys Game Jam 2025.01/Assets/Scripts/Utils/SoundManager.cs
+++ Brackeys Game Jam 2025.01/Assets/Scripts/Utils/SoundManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -9,11 +10,16 @@ public class SoundManager : MonoBehaviour
     public float SFXMult;
     public float MusicMult;
     public AudioSource UI;
-    public AudioSource SFX_Looping;
+    public AudioSource SFX_Fireplace;
+    public AudioSource SFX_Clock;
     public AudioSource SFX_OneShots;
     public AudioSource BGM;
     public AudioClip[] UIClips;
-    public Dictionary<string, AudioClip> clipDictionary = new Dictionary<string, AudioClip>();
+    public AudioClip[] LoopingSFXAssets;
+    public AudioClip[] OneShotSFXAssets;
+    public Dictionary<string, AudioClip> UIclipDictionary = new Dictionary<string, AudioClip>();
+    public Dictionary<string, AudioClip> SFXLoopsclipDictionary = new Dictionary<string, AudioClip>();
+    public Dictionary<string, AudioClip> SFXOneShotsclipDictionary = new Dictionary<string, AudioClip>();
 
     public void Awake()
     {
@@ -31,13 +37,42 @@ public class SoundManager : MonoBehaviour
 
         foreach (var clip in UIClips)
         {
-            clipDictionary[clip.name] = clip;
+            UIclipDictionary[clip.name] = clip;
         }
+       
+        foreach (var clip in LoopingSFXAssets)
+        {
+            SFXLoopsclipDictionary[clip.name] = clip;
+        }
+        
+        foreach (var clip in OneShotSFXAssets)
+        {
+            SFXOneShotsclipDictionary[clip.name] = clip;
+        }
+
+        playLoopingSFX("SFX_Clock_Ticking",SFX_Clock);
+        playLoopingSFX("SFX_Ambience_Fireplace",SFX_Fireplace);
+    }
+    public void Start()
+    {
+        
     }
 
-    public void Play_UI(string clipName)
+    public void Play_OneShot(string clipName, Dictionary<string,AudioClip> clipDictionary, AudioSource audioSource)
     {
         if (clipDictionary.TryGetValue(clipName, out AudioClip clip))
+        {
+            audioSource.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogError($"Clip with ID '{clipName}' not found!");
+        }
+
+    }
+    public void Play_UI(string clipName)
+    {
+        if (UIclipDictionary.TryGetValue(clipName, out AudioClip clip))
         {
             UI.PlayOneShot(clip);
         }
@@ -47,6 +82,21 @@ public class SoundManager : MonoBehaviour
         }
 
     }
+
+    public void playLoopingSFX(string clipName, AudioSource audioSource)
+    {
+        if (SFXLoopsclipDictionary.TryGetValue(clipName, out AudioClip clip))
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError($"Clip with ID '{clipName}' not found!");
+        }
+
+    }
+
 
 
 }
